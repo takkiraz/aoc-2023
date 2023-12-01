1862d3ec5998a0982f50b792be8e0706340ff1ba
Tolga Akkiraz <tolga.akkiraz@iits-consulting.de>
2023-12-01 11:32:27 +0100

    day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,58 @@
+enum class NUMBERS {
+    ONE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE;
+}
+
+
+fun toNumberChar(input: String): Char {
+    input.println()
+    if (input.first().isDigit()) return input.first()
+
+    return (NUMBERS.valueOf(input.uppercase()).ordinal + 1).digitToChar()
+}
+
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<String>): Long {
+        var sum = 0L
+
+        input.forEach {
+            sum += (it.first { c -> c.isDigit() }.toString() +  it.last(Char::isDigit)).toLong()
+        }
+
+        return sum
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        var sum = 0L
+        val numbersRegex = "one|two|three|four|five|six|seven|eight|nine"
+        val firstRegex = Regex("(\\d)|$numbersRegex")
+        // IDK how to reverse this regex :D
+        val lastRegex = Regex("(\\d)|" + numbersRegex.reversed())
+
+        input.forEach {
+            val first = toNumberChar(firstRegex.find(it)!!.value)
+            val last = toNumberChar(lastRegex.find(it.reversed())!!.value.reversed())
+            sum += "$first$last".toLong()
+        }
+        return sum
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInputPart1 = readInput("Day01_test_1")
+    val testInputPart2 = readInput("Day01_test_2")
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    check(part1(testInputPart1) == 142L)
+    check(part2(testInputPart2) == 281L)
+
+    val input1 = readInput("Day01_1")
+    val input2 = readInput("Day01_2")
+    part1(input1).println()
+    part2(input2).println()
 }
